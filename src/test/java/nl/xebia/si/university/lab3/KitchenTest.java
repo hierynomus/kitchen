a3b5dc82afa7214c85306b383c181c510a77c54f
hierynomus <jeroen@hierynomus.com>
2010-08-29 14:48:18 +0200

    lab3 unit test

---
+++ src/test/java/nl/xebia/si/university/lab3/KitchenTest.java
@@ -0,0 +1,40 @@
+package nl.xebia.si.university.lab3;
+
+import nl.xebia.si.university.kitchen.domain.Meal;
+import nl.xebia.si.university.kitchen.domain.Recipe;
+import nl.xebia.si.university.kitchen.domain.RecipeObjectMother;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.Message;
+import org.springframework.integration.core.MessageBuilder;
+import org.springframework.integration.core.MessageChannel;
+import org.springframework.integration.core.PollableChannel;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ */
+@ContextConfiguration(locations = "/home-dinner-flow.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class KitchenTest {
+	@Autowired
+	private MessageChannel recipes;
+
+	@Autowired
+	private PollableChannel meals;
+
+	@Test
+	public void shouldPrepareMealFromRecipe() {
+		Recipe r = RecipeObjectMother.steak();
+		recipes.send(MessageBuilder.withPayload(r).build());
+
+		final Message<Meal> message = (Message<Meal>) meals.receive();
+		final Meal meal = message.getPayload();
+		assertThat(meal.getRecipe(), is(r));
+		assertThat(meal.isDone(), is(true));
+	}
+}
