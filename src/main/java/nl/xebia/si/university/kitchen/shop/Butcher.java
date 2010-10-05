4b362440a348ba8a9a7cef78793a8291a7e558ee
hierynomus <jeroen@hierynomus.com>
2010-10-05 14:39:03 +0200

    Removed lab-5 stuff

--- src/main/java/nl/xebia/si/university/kitchen/shop/Butcher.java
+++ src/main/java/nl/xebia/si/university/kitchen/shop/Butcher.java
@@ -1,12 +1,8 @@
 package nl.xebia.si.university.kitchen.shop;
 
-import nl.xebia.si.university.kitchen.domain.GroceryBag;
 import nl.xebia.si.university.kitchen.domain.Ingredient;
 import nl.xebia.si.university.kitchen.domain.Meat;
-import nl.xebia.si.university.kitchen.domain.ShoppingList;
 import org.springframework.integration.annotation.Transformer;
-
-import java.util.List;
 
 /**
  */
@@ -16,14 +12,4 @@ public class Butcher {
 	public Meat sell(Ingredient ingredient) {
 		return new Meat(ingredient.getName(), ingredient.getAmount());
 	}
-
-    @Transformer
-    public GroceryBag<Meat> sell(ShoppingList shoppingList) {
-        GroceryBag<Meat> groceryBag = new GroceryBag<Meat>();
-        for (Ingredient ingredient : shoppingList.getItems()) {
-            groceryBag.put(sell(ingredient));
-        }
-
-        return groceryBag;
-    }
 }
